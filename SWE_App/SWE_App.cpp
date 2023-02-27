34afca64a06713b9fb3c9573c69d672ca6c5e410
Jacob A. LaMothe <lamothejacob@gmail.com>
2023-02-27 17:10:44 -0500

    Commit

---
+++ SWE_App/SWE_App.cpp
@@ -0,0 +1,19 @@
+// SWE_App.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	// wxWidgets library should be installed and compiled at C:\wxWidgets
+	// Open properties for Visual Studio project
+	// Select C/C++ -> General -> Addition Include Directories
+	// C:\wxWidgets\include;C:\wxWidgets\include\msvc
+	// Select Linker -> Additional Library Dependencies
+	// C:\wxWidgets\lib\vc_lib;C:\wxWidgets\lib\vc_x64_lib
+	// Select Linker -> System -> Subsystem
+	// Change from Console to Windows
+
+	// Once all steps above have been completed, delete this .cpp file
+	// and create your App class
+}
